dc92ef79dab34d7d8732f6a96475945b92436009
Sasidharan, Sandeep (ITCDCC) - KLM <Sandeep.Sasidharan@klm.com>
2020-01-12 19:46:01 +0100

    controller methods for crud opeartions to work with models.

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,36 @@
+import { connect } from '../../utils/db'
+import { Item } from './item.model'
+import mongoose from 'mongoose'
+
+// CRUD OPERATIONS
+
+const run = async () => {
+  await connect('mongodb://localhost:27017/api-test')
+
+  // CREATE
+  // const item = await Item.create({
+  //   name: 'Clean up',
+  //   createdBy: mongoose.Types.ObjectId(),
+  //   list: mongoose.Types.ObjectId()
+  // })
+
+  // REEAD
+  // console.log(await Item.findById(item._id).exec())
+  // console.log(await Item.find({}).exec())
+
+  // UPDATE
+  // const updated = await Item.findByIdAndUpdate(
+  //   item._id,
+  //   { name: 'eat' },
+  //   { new: true }
+  // )
+  // console.log(updated)
+
+  // REMOVE
+  // const removed = await Item.findByIdAndRemove(item._id).exec()
+  // console.log(removed)
+}
+
+run()
+
 export default {}
